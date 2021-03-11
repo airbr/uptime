6c3e5be512735694d809350ca81b2250d20a2115
flywheel <morgan@morganwebdev.com>
2021-03-10 22:09:00 -0800

    Very, very basic report creation after running the script

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 A project to create a program to read a list of URL's and check their health and return the information in a variety of formats. This intended to be a tool to promote network health and awareness with a little customization.
 
+I am kind of just hacking at this as I go.
+
 # Usage
 
 `sh uptime.sh` 
@@ -10,5 +12,11 @@ A project to create a program to read a list of URL's and check their health and
 
 In my first commits to this project, I will be committing a simple bash script which returns information read from a list of URLS.
 
-* Passed initial Shellcheck
-* Tested on several urls
+ * Passed initial Shellcheck
+ * Tested on several urls
+
+# Goals
+
+* Write results in crude/basic tabular format to a file
+* Write results to HTML
+*
--- uptime.sh
+++ uptime.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# Clear the report.txt file
+cp /dev/null report.txt;
+
+date=$(date); 
+
+printf "Report generated on: %s \n" "$date" | tee -a report.txt;
+
+
 while IFS= read -r line; do
 
     curl=$(curl -Is "$line");
     res=$?
     if test "$res" != "0"; then
-        printf "the curl command failed for %s with: %s \n" "$line" "$res";
+        printf "the curl command failed for %s with: %s \n" "$line" "$res" | tee -a report.txt;
     else
-        printf "The curl command was successful for %s. It exited with a status code of:" "$line"
-        printf "%s" "$curl" | head -n 1;
+        printf "The curl command was successful for %s. It exited with a status code of:" "$line" | tee -a report.txt;
+        printf "%s" "$curl" | head -n 1 | tee -a report.txt;
         printf "\n";
     fi
    
