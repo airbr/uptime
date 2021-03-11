d9c86a7c1e29620d904b9c68304a75351ba089dc
flywheel <morgan@morganwebdev.com>
2021-03-11 01:50:55 -0800

    Add arrays to capture success and failures, add flag for report

--- uptime.sh
+++ uptime.sh
@@ -10,10 +10,12 @@ case "$(curl -s --max-time 2 -I http://google.com | sed 's/^[^ ]*  *\([0-9]\).*/
   *) err "The network is down or very slow"; exit 1 ;;
 esac
 
-wflag=''
-while getopts 'abf:v' flag; do
+aflag='';
+fflag='';
+while getopts 'af' flag; do
   case "${flag}" in
     a) aflag='true' ;;
+    f) fflag='true' ;;
   esac
 done
 
@@ -28,16 +30,31 @@ fi
 
 printf "Report generated on: %s \n" "$date" | tee -a "$filename";
 
+success=( );
+failure=( );
+
 while IFS= read -r line; do
 
     curl=$(curl -Is "$line");
     res=$?
     if test "$res" != "0"; then
         printf "the curl command failed for %s with: %s \n" "$line" "$res" | tee -a "$filename";
+        failure+=("$line");
     else
         printf "The curl command was successful for %s. It exited with a status code of:" "$line" | tee -a "$filename";
         printf "%s" "$curl" | head -n 1 | tee -a "$filename";
         printf "\n";
+        success+=("$line");
     fi
 
 done < urls.txt
+
+if [[ -n $fflag ]]; then
+cp /dev/null reports/formatted-report.txt;
+ printf "SUCCESS:\n"   
+ printf '%s\n' "${success[@]}" | tee -a "reports/formatted-report.txt";
+ printf "\n" | tee -a "reports/formatted-report.txt";
+ printf "FAILURE:\n" | tee -a "reports/formatted-report.txt";
+ printf '%s\n' "${failure[@]}" | tee -a "reports/formatted-report.txt";
+fi
+
