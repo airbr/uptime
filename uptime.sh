824581c6ea2fcb7ecd04e80ac09be729ea7c30c2
Morgan Murrah <morgan@morganwebdev.com>
2021-06-02 23:22:55 -0700

    Modularizing it down

--- uptime.sh
+++ uptime.sh
@@ -1,62 +1,39 @@
 #!/bin/env bash
-set -euo pipefail;
+set -uo pipefail;
+# set -e is aspired to for this script, currently relies on capturing failed Curl calls
 
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
+# A very crude way to check connectivity is to curl google.com
 case "$(curl -s --max-time 2 -I https://www.google.com | sed 's/^[^ ]*  *\([0-9]\).*/\1/; 1q')" in
   [23]) echo "HTTP connectivity is up";;
   5) echo "The web proxy won't let us through";;
   *) err "The network is down or very slow"; exit 1 ;;
 esac
 
-aflag='';
-fflag='';
-while getopts 'af' flag; do
-  case "${flag}" in
-    a) aflag='true' ;;
-    f) fflag='true' ;;
-    *) err "Unexpected flag"; exit 1 ;;
-  esac
-done
-
-date=$(date); 
-if [[ -n $aflag ]]; then
-    filename="reports/$date-report.txt"
-else
-  # Clear the report.txt file
-    filename="reports/temp-report.txt"
-    cp /dev/null reports/temp-report.txt;
-fi
-
-printf "Report generated on: %s \n" "$date" | tee -a "$filename";
-
-success=( );
-failure=( );
+printf "URL STATUS UP";
+printf "\n";
 
 while IFS= read -r line; do
 
     curl=$(curl -Is --max-time 15 "$line");
     res=$?
+  
     if test "$res" != "0"; then
-        printf "the curl command failed for %s with: %s \n" "$line" "$res" | tee -a "$filename";
-        failure+=("$line");
-    else
-        printf "The curl command was successful for %s. It exited with a status code of:" "$line" | tee -a "$filename";
-        printf "%s" "$curl" | head -n 1 | tee -a "$filename";
-        printf "\n";
-        success+=("$line");
-    fi
+      printf "$line $res No";
+      printf "\n";
 
+        # printf "the curl command failed for %s with: %s \n" "$line" "$res";
+    else
+      printf "$line $res Yes";
+      printf "\n";
+
+        # printf "The curl command was successful for %s. It exited with a status code of:" "$line";
+        # printf "%s" "$curl" | head -n 1;
+        # printf "\n";
+    fi
 done < urls.txt
 
-if [[ -n $fflag ]]; then
-cp /dev/null reports/formatted-report.txt;
- printf "SUCCESS:\n"   
- printf '%s\n' "${success[@]}" | tee -a "reports/formatted-report.txt";
- printf "\n" | tee -a "reports/formatted-report.txt";
- printf "FAILURE:\n" | tee -a "reports/formatted-report.txt";
- printf '%s\n' "${failure[@]}" | tee -a "reports/formatted-report.txt";
-fi
 
